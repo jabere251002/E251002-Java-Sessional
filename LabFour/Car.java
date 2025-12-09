373372398264261f72e835051559aa25e77c0f51
AL JABER <aljaber54325@gmail.com>
2025-12-09 20:05:10 +0600

    Create Car.java

---
+++ LabFour/Car.java
@@ -0,0 +1,20 @@
+class Car {
+    String brand;
+    String model;
+    double price;
+    Car(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+
+    }
+    void displayCar() {
+        System.out.println("Car Brand: " + brand);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Price: BDT " + price);
+    }
+    public static void main(String[] args) {
+        Car myCar = new Car("CCE", "CCE Car 1", 542000);
+        myCar.displayCar();
+    }
+}
